de54ce80492a84713c6b7553da07b452e5695a7a
Hemal Shah <hemal.shah1996@gmail.com>
2016-04-27 16:03:25 +0530

    Chart part-1

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ App created as a part of Android Nanodegree course available at www.udacity.com 
 <hr>
 <ol>
     <li>Showing different error messages to user!</li>
+    <li>created sample graph layout using MPCharting Library.</li>
 </ol>
--- app/src/main/java/com/sam_chordas/android/stockhawk/ui/StockDetails.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/ui/StockDetails.java
@@ -1,21 +1,27 @@
 package com.sam_chordas.android.stockhawk.ui;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
 import com.sam_chordas.android.stockhawk.R;
-
-import org.hogel.android.linechartview.LineChartView;
-
 import java.util.ArrayList;
-import java.util.List;
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * Created by hemal on 21/4/16.
  */
 public class StockDetails extends AppCompatActivity {
 
-    private static final double MAX_Y = 100;
-    private LineChartView chartView;
+    @Bind(R.id.lineChart_activity_line_graph)
+    LineChart lineChart;
 
 
     @Override
@@ -23,15 +29,37 @@ public class StockDetails extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_line_graph);
 
+        ButterKnife.bind(this);
 
-        chartView  = (LineChartView) findViewById(R.id.linechart);
-        chartView.setManualMinY(0);
+        ArrayList<Entry> entries = new ArrayList<>();
+        ArrayList<String> xvalues = new ArrayList<>();
 
-        List<LineChartView.Point> points = new ArrayList<>();
-        for (int i = 0; i < 50; i++) {
-            int y = (int) (Math.random() * MAX_Y);
-            points.add(new LineChartView.Point(i, y));
+        for(int i = 0; i < 10; i++){
+
+            int yValue = (i%2==0)? (i*10) : (i*3);
+
+            xvalues.add("Month " + (i+1));
+            entries.add(new Entry(yValue*1f, i));
         }
-        chartView.setPoints(points);
+
+
+        XAxis xAxis = lineChart.getXAxis();
+        xAxis.setLabelsToSkip(2);
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setTextSize(12f);
+        YAxis left = lineChart.getAxisLeft();
+        left.setEnabled(true);
+        left.setLabelCount(5, true);
+
+        lineChart.getAxisRight().setEnabled(false);
+
+        lineChart.getLegend().setTextSize(16f);
+
+        LineDataSet dataSet = new LineDataSet(entries, "Stock Name");
+        LineData lineData = new LineData(xvalues ,dataSet);
+        lineChart.setBackgroundColor(Color.WHITE);
+        lineChart.setData(lineData);
+        lineChart.setDescription("Hello this is some description.!");
+        lineChart.animateXY(3000, 3000);
     }
 }
